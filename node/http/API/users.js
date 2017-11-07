6c5b528197af0355aacf55d346786d7268367370
chenjisheng_cs@southernfund.com <chenjisheng_cs@southernfund.com>
2017-11-07 17:19:28 +0800

    node本地服务器实例

---
+++ node/http/API/users.js
@@ -0,0 +1,28 @@
+//const expres=require('express');
+//var express = require('express');
+//var router = express.Router()
+let APIUsers={};
+APIUsers.data=[
+    { id: 100, name: '张三丰', pass:'zsf', age: 20, ico:'/imgs/0.jpg', en_name:'ZhangSanFeng', money:100 },
+    { id: 200, name: '许文强', pass:'xwq', age: 30, ico: '/imgs/1.jpg', en_name:'XueWenQiang', money:200 },
+    { id: 300, name: '杨过', pass:'yg', age: 40, ico: '/imgs/2.jpg', en_name:'YangGuo', money:300 }
+];
+APIUsers.api={
+	'get_all_users':{
+		method:"get|post",
+		handle:function(req,res){
+			let result=APIUsers.data;
+			res.send(result);
+		}
+	}
+}
+APIUsers.handle=(req,res)=>{
+	var pathname = req._parsedUrl.pathname, apiname = pathname.substr(pathname.lastIndexOf('/') + 1),
+        Url = APIUsers.api[apiname], method = req.method.toLowerCase();
+    if (Url && Url.method.split("|").toString().indexOf(method)>-1) {
+        Url.handle(req, res);
+    } else {
+        res.send({err:400});
+    }
+}
+module.exports=APIUsers;
---
+++ node/http/server.js
@@ -0,0 +1,15 @@
+const path=require('path');
+const express=require('express');
+const request=require('request');
+const APIUsers=require('./API/users');
+
+const app=express();
+
+//指定静态资源目录
+app.use(express.static(path.join(__dirname, 'src')));
+//截取前端请求路径为/api的
+app.use('/api',APIUsers.handle);
+const server=app.listen(2001,function(){
+	const port=server.address().port;
+	console.log('服务已启动,应用访问地址为:localhost:'+port);
+})
