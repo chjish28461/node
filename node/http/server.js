4612accc66f40214ed5a12e12be30d0038a1204f
chjish28461@163.com <huy13767713885u>
2018-03-09 10:39:42 +0800

    修改端口号为80

--- node/http/server.js
+++ node/http/server.js
@@ -15,7 +15,7 @@ app.use(express.static(path.join(__dirname, 'src')));
 //截取前端请求路径为/api的
 app.use('/api',APIUsers.handle);
 //监听服务端口
-const server=app.listen(2001,function(){
+const server=app.listen(80,function(){
 	const port=server.address().port;
 	console.log('服务已启动,应用访问地址为:localhost:'+port);
 })
